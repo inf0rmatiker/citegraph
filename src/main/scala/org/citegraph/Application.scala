5b370b636aee2dcf965f2c788df5d5ec344bd57f
Caleb Carlson <ccarlson355@gmail.com>
2022-01-31 20:54:42 -0700

    Log input args

--- src/main/scala/org/citegraph/Application.scala
+++ src/main/scala/org/citegraph/Application.scala
@@ -1,5 +1,7 @@
 package org.citegraph
 
+import org.apache.spark.sql.SparkSession
+
 object Application {
 
   def printArgs(args: Array[String]): Unit = {
@@ -13,8 +15,7 @@ object Application {
     println("USAGE")
     println("\tBuild:\n\t\tsbt package")
     println("\tSubmit as JAR to Spark cluster:\n\t\t$SPARK_HOME/bin/spark-submit <submit_options> \\")
-    println("\t\ttarget/scala-2.13/citegraph_2.13-0.1.jar <hdfs_file>")
-    println("\t\trequires option csv_directory needs to have path to where the data csv files are")
+    println("\t\ttarget/scala-2.13/citegraph_2.13-0.1.jar <hdfs_input_dir> <hdfs_output_dir>")
     println()
   }
 
@@ -25,6 +26,17 @@ object Application {
       System.exit(1)
     }
 
+    // Make a Spark Session
+    val sparkSession: SparkSession = SparkSession.builder
+      .appName("CiteGraph")
+      .getOrCreate()
+
+    val inputDirectory: String = args(0)
+    val outputDirectory: String = args(1)
+
+    printf("inputDirectory: %s, outputDirectory: %s\n", inputDirectory, outputDirectory)
+
+    sparkSession.close()
   }
 
 }
