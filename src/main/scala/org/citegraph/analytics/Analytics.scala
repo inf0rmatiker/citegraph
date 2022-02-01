010287323a6c061cbcc316e9a14eed4caf16c19c
Caleb Carlson <ccarlson355@gmail.com>
2022-02-01 14:46:40 -0700

    Add comment with result DF sample

--- src/main/scala/org/citegraph/analytics/Analytics.scala
+++ src/main/scala/org/citegraph/analytics/Analytics.scala
@@ -8,10 +8,22 @@ class Analytics(sparkSession: SparkSession, citationsDF: DataFrame, publishedDat
 
   def findDensitiesByYear(): Unit = {
 
-    // Find node density by year - group by year and count records
+    /*
+    Find node density by year - group by year and count records:
+      +----+-----+
+      |year|count|
+      +----+-----+
+      |1997| 4252|
+      |1994| 2894|
+      |1996| 3939|
+      |... |...  |
+      +----+-----+
+     */
     publishedDatesDF.groupBy(col("year"))
       .count()
-      .show(10)
+      .show()
+
+
   }
 
 
