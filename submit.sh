84d8f3b8f23ba8fb68627a8266b41175777883f8
Caleb Carlson <ccarlson355@gmail.com>
2022-02-01 09:37:07 -0700

    Update submit script

--- submit.sh
+++ submit.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 function print_usage {
-  echo -e "USAGE\n\tsubmit.sh <cluster_manager> <input_data_directory> <output_directory>"
-  echo -e "\nEXAMPLES\n"
-  echo -e "\tsubmit.sh yarn hdfs://bismarck:30201/cs535/data hdfs://bismarck:30201/cs535/output\n"
-  echo -e "\tsubmit.sh local[*] hdfs://bismarck:30201/cs535/data hdfs://bismarck:30201/cs535/output\n"
-  echo -e "\tsubmit.sh spark://bismarck:30216 hdfs://bismarck:30201/cs535/data hdfs://bismarck:30201/cs535/output\n"
+  echo -e "USAGE\n\tsubmit.sh <input_data_directory> <output_directory>"
+  echo -e "\nEXAMPLE\n"
+  echo -e "\tsubmit.sh hdfs://bismarck:30201/cs535/data hdfs://bismarck:30201/cs535/output\n"
 }
 
 if [[ $# -eq 3 ]]; then
   echo -e "Submitting Spark Job...\n"
   HADOOP_CONF_DIR="$HOME/hadoop" "${SPARK_HOME}"/bin/spark-submit \
     --class org.citegraph.Application \
-    --master "$1" \
+    --master spark://bismarck:9079 \
     --deploy-mode cluster \
     --driver-memory 4g \
     --executor-memory 2g \
