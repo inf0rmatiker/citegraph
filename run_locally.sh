6debb263e7648085986e59690b3a16c05c40f681
Caleb Carlson <ccarlson355@gmail.com>
2022-02-11 12:46:02 -0700

    Run locally uses inputs for dirs

--- run_locally.sh
+++ run_locally.sh
@@ -4,13 +4,16 @@ function print_usage {
   echo -e "USAGE\n\run_locally.sh"
 }
 
-if [[ $# -eq 0 ]]; then
+if [[ $# -eq 2 ]]; then
+
+  INPUT_DIR=$1
+  OUTPUT_DIR=$2
 
   echo -e "Submitting Spark Job to local master...\n"
   "${SPARK_HOME}"/bin/spark-submit \
     --class org.citegraph.Application \
     --master "local[*]" \
-    target/scala-2.12/citegraph_2.12-0.1.jar "--testing" "data/testing" "data/testing"
+    target/scala-2.12/citegraph_2.12-0.1.jar "--testing" "$INPUT_DIR" "$OUTPUT_DIR"
 else
   print_usage
 fi
